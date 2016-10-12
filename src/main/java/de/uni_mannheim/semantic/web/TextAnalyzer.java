65a140cf3a44e6a1b72dfc7d76cbaec9119a7ffd
Zahlii <niklasfruehauf@gmail.com>
2016-10-12 15:17:09 +0200

    Explanations for POS Tagging

--- src/main/java/de/uni_mannheim/semantic/web/TextAnalyzer.java
+++ src/main/java/de/uni_mannheim/semantic/web/TextAnalyzer.java
@@ -87,13 +87,48 @@ public class TextAnalyzer {
 		}
 		
 		// https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+		
+		/*
+		S -> Simple declarative clause, i.e. one that is not introduced by a (possible empty) subordinating conjunction or a wh-word and that does not exhibit subject-verb inversion.
+		SBAR -> Clause introduced by a (possibly empty) subordinating conjunction.
+		SBARQ -> Direct question introduced by a wh-word or a wh-phrase. Indirect questions and relative clauses should be bracketed as SBAR, not SBARQ.
+		SINV -> Inverted declarative sentence, i.e. one in which the subject follows the tensed verb or modal.
+		SQ -> Inverted yes/no question, or main clause of a wh-question, following the wh-phrase in SBARQ.
+		ADJP -> Adjective Phrase.
+		ADVP -> Adverb Phrase.
+		CONJP -> Conjunction Phrase.
+		FRAG -> Fragment.
+		INTJ -> Interjection. Corresponds approximately to the part-of-speech tag UH.
+		LST -> List marker. Includes surrounding punctuation.
+		NAC -> Not a Constituent; used to show the scope of certain prenominal modifiers within an NP.
+		NP -> Noun Phrase.
+		NX -> Used within certain complex NPs to mark the head of the NP. Corresponds very roughly to N-bar
+		PP -> Prepositional Phrase.
+		PRN -> Parenthetical.
+		PRT -> Particle. Category for words that should be tagged RP.
+		QP -> Quantifier Phrase (i.e. complex measure/amount phrase); used within NP.
+		RRC -> Reduced Relative Clause.
+		UCP -> Unlike Coordinated Phrase.
+		VP -> Verb Phrase.
+		WHADJP -> Wh-adjective Phrase. Adjectival phrase containing a wh-adverb, as in how hot.
+		WHAVP -> Wh-adverb Phrase. Introduces a clause with an NP gap. May be null (containing the 0 complementizer) or lexical, containing a wh-adverb such as how or why.
+		WHNP -> Wh-noun Phrase. Introduces a clause with an NP gap. May be null (containing the 0 complementizer) or lexical, containing some wh-word, e.g. who, which book, whose daughter, none of which, or how many leopards.
+		WHPP -> Wh-prepositional Phrase. Prepositional phrase containing a wh-noun phrase (such as of which or by whose authority) that either introduces a PP gap or is contained by a WHNP.
+		X -> Unknown, uncertain, or unbracketable. X is often used for bracketing typos and in bracketing the...the-constructions.
+		 */
 		Parse x =_parser.parse(p);
-		System.out.println(_originalText);
-		x.show();
+		
+
+		
+    }
+    
+    private void analyzeParsedTree(Parse tree) {
+    	System.out.println(_originalText);
+		tree.show();
 		System.out.println();
     }
     
-    private static boolean isCandidateForEntity(String t) {
+    private boolean isCandidateForNamedEntity(String t) {
     	if(t.contains(" ") || t.contains("_"))
     		return false;
     	String s = t.substring(0,1);
@@ -121,7 +156,7 @@ public class TextAnalyzer {
 			String successfulLookupResult = null;
 			
 			// current token a noun?
-			if(isCandidateForEntity(t)) {			
+			if(isCandidateForNamedEntity(t)) {			
 						
 				// create up to maxLength Lookups
 				for(int j=i;j<Math.min(l,i+4);j++) {
