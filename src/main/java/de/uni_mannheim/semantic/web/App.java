741293a33a878edcd881735bf3776ef945ea6475
Zahlii <niklasfruehauf@gmail.com>
2016-10-12 09:57:44 +0200

    Initial Commit Maven

---
+++ src/main/java/de/uni_mannheim/semantic/web/App.java
@@ -0,0 +1,13 @@
+package de.uni_mannheim.semantic.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
